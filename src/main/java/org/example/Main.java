5af64bccd799a5d7d1f4c0fa026b4d647112cc2a
soulasphyxia <maslyuk03@bk.ru>
2023-10-23 16:43:12 +0700

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,29 @@ package org.example;
 
 import org.example.labs.Lab1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //lab 1
-        Lab1.solve();
+        System.out.println("""
+                Программная реализация лабораторных рабор по ОТПИ:
+                1.Лаб.работа №1 "RLE-алгоритм"
+                2.Лаб.работа №2 "Кодирование методом Шеннона-Фано"
+                3.Лаб.работа №3 "Кодирование методом Шеннона-Фано по блокам"
+                """);
+        boolean exit = false;
+        while(!exit){
+            System.out.print("Введите опцию: ");
+            Scanner sc = new Scanner(System.in);
+            int option = sc.nextInt();
+            switch(option){
+                case 1 -> Lab1.solve();
+                case 2 -> System.out.println("lab 2");
+                case 3 -> System.out.println("lab 3");
+                default -> exit = true;
+            }
+        }
 
 
-
-
-        //lab 2
-
-        //lab 3
-
-        //lab 4
-
-
-        //lab 5
     }
 }
