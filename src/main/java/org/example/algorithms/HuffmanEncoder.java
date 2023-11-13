44445b0f699ca5d68d675d64eababa3856841b7b
soulasphyxia <maslyuk03@bk.ru>
2023-11-14 03:26:22 +0700

    commit

--- src/main/java/org/example/algorithms/HuffmanEncoder.java
+++ src/main/java/org/example/algorithms/HuffmanEncoder.java
@@ -13,22 +13,24 @@ public class HuffmanEncoder {
 
     public HuffmanEncoder(ArrayList<Letter> letters){
         this.letters = letters;
+        this.codes = new HashMap<>();
     }
 
 
     public HashMap<Letter, String> encode(){
         //ПОПРОБОВАТЬ PRIORITY QUEUE
         PriorityQueue<Letter> pq = new PriorityQueue<>();
+        TreeMap<Letter, Double> map = new TreeMap<>();
         pq.addAll(letters);
         while(pq.size() != 1){
             Letter l1 = pq.poll();
             Letter l2 = pq.poll();
-            System.out.println(l1 + " " + l2);
-            l1.setProb(l1.getProb() + l2.getProb());
-            pq.add(l1);
+            map.put(l1,l1.getProb());
+            map.put(l2,l2.getProb());
+            pq.add(new Letter(l1.getValue()+l2.getValue(),l1.getProb()+ l2.getProb()));
         }
-        System.out.println(pq.peek().getProb());
 
+        System.out.println(map);
 
 
         return codes;
